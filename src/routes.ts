5383f7aa854825684a15686e83920faa3ca4375c
HuyTong03 <tongphuochuy072019@gmail.com>
2024-04-18 17:35:11 +0700

    pageNotFound

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,9 @@
- import Login from "./pages/Login";
- import Home from "./pages/Home/Home";
-import NotFound from "./pages/NotFound";
-import Register from "./pages/Register";
- const routes = [
+import Login from './pages/Login';
+import Home from './pages/Home/Home';
+import NotFound from './pages/NotFound';
+import Register from './pages/Register';
+import EmailVerifier from './pages/EmailConfirm/EmailVerifier';
+const routes = [
     {
         path: '/login',
         page: Login,
@@ -11,20 +12,24 @@ import Register from "./pages/Register";
         roles: ['admin'],
     },
     {
+        path: '/verify',
+        page: EmailVerifier,
+    },
+    {
         path: '/register',
         page: Register,
         isShowHeader: true,
         isPrivate: true,
         roles: ['admin'],
     },
-     {
+    {
         path: '/',
         page: Home,
         isShowHeader: true,
         isPrivate: true,
         roles: ['admin'],
     },
-    
+
     {
         path: '*',
         page: NotFound,
