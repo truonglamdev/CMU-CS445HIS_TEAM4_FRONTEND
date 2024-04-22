0313ba7b79bdccfb14c3e5b007fa24f6d088721b
truonglamdev <truonglamdev070303@gmail.com>
2024-04-22 15:00:49 +0700

    Page not found

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,16 @@
+import classNames from 'classnames/bind';
+import styles from './NotFound.module.scss';
+const cx = classNames.bind(styles);
 function NotFound() {
-  return (
-    <div>NotFound</div>
-  )
+    return (
+        <div className={cx('not-found-container', 'wrapper')}>
+            <h1>404</h1>
+            <div>Oops ! Page not found</div>
+            <p>
+                <a href="/">Return Home</a>
+            </p>
+        </div>
+    );
 }
 
-export default NotFound
+export default NotFound;
